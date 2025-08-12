f0c2fecf16692a6a2a865a6623078eb7f7f25f60
Rajan Sharma Mac <rajankumarsharma681@gmail.com>
2025-08-12 21:18:12 +0530

    --

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script setup>
-import Navbar from '/components/Navbar.vue'
-import Footer from '/components/Footer.vue'
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
 </script>
